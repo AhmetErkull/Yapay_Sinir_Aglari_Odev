21256f771d7cb068ce1be95c1bd72d24fa9bba38
mefistokrat <64774935+mefistokrat@users.noreply.github.com>
2024-06-13 09:00:17 +0300

    first commit

---
+++ yapaysiniraglariodevi/yapaysiniraglariodevi/Algoritma.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.IO;
+
+namespace yapaysiniraglariodevi
+{
+    public class Algoritma
+    {
+        private int girdi = 35;
+        private int ara = 10;
+        private int cikti = 5;
+        private double[,] girdi_ara_agirlik;
+        private double[,] ara_cikti_agirlik;
+        private double[,] girdi_ara_agirlik_degisim;
+        private double[,] ara_cikti_agirlik_degisim;
+        private double[] ara_bias_agirlik;
+        private double[] cikti_bias_agirlik;
+        private double[] ara_bias_degisim;
+        private double[] cikti_bias_degisim;
+        private double ogrenme_katsayisi;
+        private double momentum;
+
+        public Algoritma()
+        {
+            Random random = new Random();
+            girdi_ara_agirlik = new double[girdi, ara];
+            ara_cikti_agirlik = new double[ara, cikti];
+            girdi_ara_agirlik_degisim = new double[girdi, ara];
+            ara_cikti_agirlik_degisim = new double[ara, cikti];
+            ara_bias_agirlik = new double[ara];
+            cikti_bias_agirlik = new double[cikti];
+            ara_bias_degisim = new double[ara];
+            cikti_bias_degisim = new double[cikti];
+
+          
+            for (int i = 0; i < girdi; i++)
+            {
+                for (int j = 0; j < ara; j++)
+                {
+                    girdi_ara_agirlik[i, j] = random.NextDouble() * 2 - 1;
+                    girdi_ara_agirlik_degisim[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < ara; i++)
+            {
+                ara_bias_agirlik[i] = random.NextDouble() * 2 - 1;
+                ara_bias_degisim[i] = 0;
+            }
+
+            
+            for (int i = 0; i < ara; i++)
+            {
+                for (int j = 0; j < cikti; j++)
+                {
+                    ara_cikti_agirlik[i, j] = random.NextDouble() * 2 - 1;
+                    ara_cikti_agirlik_degisim[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < cikti; i++)
+            {
+                cikti_bias_agirlik[i] = random.NextDouble() * 2 - 1;
+                cikti_bias_degisim[i] = 0;
+            }
+        }
+
+
+
+        public double[] Tahmin(int[,] girisler)
+        {
+            double[] arakatmanciktisi = new double[ara];
+            double[] ciktikatmanciktisi = new double[cikti];
+            int[] giris = new int[girdi];
+
+            for (int i = 0; i < girisler.GetLength(0); i++)
+            {
+                for (int j = 0; j < girisler.GetLength(1); j++)
+                {
+                    giris[i * 5 + j] = girisler[i, j];
+                }
+            }
+
+            
+            for (int i = 0; i < ara; i++)
+            {
+                double toplam = ara_bias_agirlik[i];
+              
+                for (int j = 0; j < girdi; j++)
+                {
+                    toplam += giris[j] * girdi_ara_agirlik[j, i];
+                }
+                arakatmanciktisi[i] = Sigmoid(toplam);
+            }
+
+            
+            for (int i = 0; i < cikti; i++)
+            {
+                double toplam = cikti_bias_agirlik[i];
+           
+                for (int j = 0; j < ara; j++)
+                {
+                    toplam += arakatmanciktisi[j] * ara_cikti_agirlik[j, i];
+                }
+                ciktikatmanciktisi[i] = Sigmoid(toplam);
+            }
+
+            return ciktikatmanciktisi;
+        }
+
+
+        public void Egit(int[,,] girisler, int[,] hedef, double ogrenme_katsayisi, double momentum, double epsilon)
+        {
+            this.ogrenme_katsayisi = ogrenme_katsayisi;
+            this.momentum = momentum;
+
+            double toplamhata = double.MaxValue;
+            int[] giris = new int[girdi];
+            double[] arakatman_ciktisi = new double[ara];
+            double[] ciktikatmani_ciktisi = new double[cikti];
+            double[] ciktikatmani_hatasi = new double[cikti];
+            double[] arakatman_hatasi = new double[ara];
+
+            while (epsilon < toplamhata)
+            {
+                toplamhata = 0;
+                for (int x = 0; x < girisler.GetLength(0); x++)
+                {
+                    for (int i = 0; i < girisler.GetLength(1); i++)
+                    {
+                        for (int j = 0; j < girisler.GetLength(2); j++)
+                        {
+                            giris[i * 5 + j] = girisler[x, i, j];
+                        }
+                    }
+
+                    // Netleri hesapladım
+                    for (int j = 0; j < ara; j++)
+                    {
+                        double toplam = ara_bias_agirlik[j];
+                        for (int k = 0; k < girdi; k++)
+                        {
+                            toplam += giris[k] * girdi_ara_agirlik[k, j];
+                        }
+                        arakatman_ciktisi[j] = Sigmoid(toplam);
+                    }
+
+                    for (int j = 0; j < cikti; j++)
+                    {
+                        double toplam = cikti_bias_agirlik[j];
+                        for (int k = 0; k < ara; k++)
+                        {
+                            toplam += arakatman_ciktisi[k] * ara_cikti_agirlik[k, j];
+                        }
+                        ciktikatmani_ciktisi[j] = Sigmoid(toplam);
+                    }
+
+                    // Hata değerlerini hesapladım
+                    for (int j = 0; j < cikti; j++)
+                    {
+                        ciktikatmani_hatasi[j] = hedef[x, j] - ciktikatmani_ciktisi[j]; //ağın hatası
+                        double hata = ciktikatmani_hatasi[j];
+                        toplamhata += 0.5 * Math.Pow(hata, 2);
+                    }
+
+                    for (int j = 0; j < ara; j++)
+                    {
+                        double toplam = 0;
+                        for (int k = 0; k < cikti; k++)
+                        {
+                            toplam += ciktikatmani_hatasi[k] * ara_cikti_agirlik[j, k];
+                        }
+                        // Sigmoidin türevi
+                        arakatman_hatasi[j] = arakatman_ciktisi[j] * (1 - arakatman_ciktisi[j]) * toplam;
+                    }
+
+                    // Değişimleri ayarladım
+                    for (int j = 0; j < ara; j++)
+                    {
+                        ara_bias_degisim[j] = momentum * ara_bias_degisim[j] + ogrenme_katsayisi * arakatman_hatasi[j];
+                        ara_bias_agirlik[j] += ara_bias_degisim[j];
+
+                        for (int k = 0; k < cikti; k++)
+                        {
+                            cikti_bias_degisim[k] = momentum * cikti_bias_degisim[k] + ogrenme_katsayisi * ciktikatmani_hatasi[k];
+                            cikti_bias_agirlik[k] += cikti_bias_degisim[k];
+
+
+                            double degisim = momentum * ara_cikti_agirlik_degisim[j, k] + ogrenme_katsayisi * ciktikatmani_hatasi[k] * (1 - ciktikatmani_ciktisi[k]) * ciktikatmani_ciktisi[k] * arakatman_ciktisi[j];
+
+                            // double degisim = momentum * ara_cikti_agirlik_degisim[j, k] + ogrenme_katsayisi * ciktikatmani_hatasi[k] * arakatman_ciktisi[j];
+                            ara_cikti_agirlik[j, k] += degisim;
+                            ara_cikti_agirlik_degisim[j, k] = degisim;
+                        }
+                    }
+
+                    for (int j = 0; j < girdi; j++)
+                    {
+
+                        for (int k = 0; k < ara; k++)
+                        {
+                            double degisim = momentum * girdi_ara_agirlik_degisim[j, k] + ogrenme_katsayisi * arakatman_hatasi[k] * giris[j];
+                            girdi_ara_agirlik[j, k] += degisim;
+                            girdi_ara_agirlik_degisim[j, k] = degisim;
+                        }
+                    }
+                }
+                Console.WriteLine(toplamhata);
+                Console.WriteLine("Eğitiliyor...");
+            }
+            Console.WriteLine("Eğitim bitti.");
+        }
+
+
+        private double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+
+
+
+
+        public void Kaydet(string dosyaAdi ="deneme.txt")
+        {
+            if (string.IsNullOrEmpty(dosyaAdi))
+            {
+                Random rnd = new Random();
+                dosyaAdi = "deneme" + rnd.NextDouble()+ ".txt";
+            }
+            else
+            {
+                dosyaAdi += ".txt";
+            }
+            using (StreamWriter writer = new StreamWriter(dosyaAdi))
+            {
+                
+                for (int i = 0; i < girdi; i++)
+                {
+                    for (int j = 0; j < ara; j++)
+                    {
+                        writer.WriteLine(girdi_ara_agirlik[i, j]);
+                    }
+                }
+
+               
+                for (int i = 0; i < ara; i++)
+                {
+                    for (int j = 0; j < cikti; j++)
+                    {
+                        writer.WriteLine(ara_cikti_agirlik[i, j]);
+                    }
+                }
+
+                for (int i = 0; i < ara; i++)
+                {
+                    writer.WriteLine(ara_bias_agirlik[i]);
+                }
+
+                for (int i = 0; i < cikti; i++)
+                {
+                    writer.WriteLine(cikti_bias_agirlik[i]);
+                }
+
+
+                writer.Close();
+            }
+        }
+
+        public void Yukle(string dosyaAdi)
+        {
+            using (StreamReader reader = new StreamReader(dosyaAdi))
+            {
+              
+                for (int i = 0; i < girdi; i++)
+                {
+                    for (int j = 0; j < ara; j++)
+                    {
+                        girdi_ara_agirlik[i, j] = double.Parse(reader.ReadLine());
+                    }
+                }
+
+               
+                for (int i = 0; i < ara; i++)
+                {
+                    for (int j = 0; j < cikti; j++)
+                    {
+                        ara_cikti_agirlik[i, j] = double.Parse(reader.ReadLine());
+                    }
+                }
+
+                for (int i = 0; i < ara; i++)
+                {
+                    ara_bias_agirlik[i] = double.Parse(reader.ReadLine());
+                }
+
+                // Ara-cikti bias değerlerini yükle
+                for (int i = 0; i < cikti; i++)
+                {
+                    cikti_bias_agirlik[i] = double.Parse(reader.ReadLine());
+                }
+
+                reader.Close();
+            }
+        }
+
+
+    }
+}
---
+++ yapaysiniraglariodevi/yapaysiniraglariodevi/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yapaysiniraglariodevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Algoritma a = new Algoritma();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+          
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,,] egitim = new int[10, 7, 5] {
+        {
+            {0,0,1,0,0},
+            {0,1,0,1,0},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1}
+        },
+
+        {
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1}
+        },
+         {
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1}
+        },
+
+        {
+            {1,1,1,1,0},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,0},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,0}
+        },
+        {
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,1}
+        },
+        {
+            {0,0,1,1,1},
+            {0,1,0,0,0},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {0,1,0,0,0},
+            {0,0,1,1,1}
+        },
+           {
+            {1,1,1,1,1},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {1,1,1,1,1}
+        },
+        {
+            {1,1,1,0,0},
+            {1,0,0,1,0},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,0,0,1,0},
+            {1,1,1,0,0}
+        },
+         {
+            {1,1,1,1, 1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,1}
+        },
+        {
+            {1,1,1,1,1},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {1,1,1,1,1},
+            {1,0,0,0,0},
+            {1,0,0,0,0},
+            {1,1,1,1,1}
+        }
+    };
+
+            int[,] istenenCıktı = new int[10, 5] {
+        {0,0,0,0,1},
+         {0,0,0,0,1},
+          {0,0,0,0,1},
+        {0,0,0,1,0},
+         {0,0,0,1,0},
+        {0,0,1,0,0},
+          {0,0,1,0,0},
+        {0,1,0,0,0},
+          {0,1,0,0,0},
+        {1,0,0,0,0}
+    };
+
+            a.Egit(egitim, istenenCıktı, 0.1, 0.9, (double)numericUpDown1.Value);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            int[,] deneme =
+         {
+                   {1,1,1,1,1},
+                   {1,1,0,1,1},
+                   {1,0,0,0,1},
+                   {1,1,1,1,1},
+                   {1,0,0,0,1},
+                   {1,0,0,0,1},
+                   {1,0,0,0,1}
+               };
+
+            int[,] dizi = new int[7, 5];
+
+            int index = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                  
+                    Control button = Controls.Find("button" + (index+3), true).FirstOrDefault();
+                    if (button != null && button.BackColor == Color.White)
+                        dizi[i, j] = 0;
+                    else
+                        dizi[i, j] = 1;
+                    index++;
+                }
+
+            }
+
+            double enbuyuk = 0;
+            double gecici;
+            int enbuyukindex = 0;
+            for (int i = 0; i < 5; i++)
+            {
+               
+                Label lbl= Controls.Find("lbl" + (i+ 1), true).FirstOrDefault() as Label;
+                lbl.Text = a.Tahmin(dizi)[i].ToString();
+                gecici = Convert.ToDouble(lbl.Text);
+
+                if (enbuyuk<gecici)
+                {
+                    enbuyuk = gecici;
+                    enbuyukindex = i;
+                }
+            }
+            Label label = Controls.Find("label" + (enbuyukindex + 7), true).FirstOrDefault() as Label;
+            label1.Text = label.Text;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+     
+            if (btn.BackColor == Color.White)
+            {
+                btn.BackColor = Color.Black;
+            }
+            else
+            {
+                btn.BackColor = Color.White;
+            }
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            a.Kaydet(textBox1.Text);
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Metin Dosyaları (*.txt)|*.txt";
+            dialog.Title = "Ağırlıkları ve Bias Değerlerini Yükle";
+
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string dosyaAdi = dialog.FileName;
+                a.Yukle(dosyaAdi);
+
+            }
+        }
+        }
+}
